0ce6ced11156110526a4da5b1a2da86e39ae452d
Andrew Alverson <aja@uark.edu>
2023-04-12 09:19:22 -0500

    splits the line on tab; inserts length of feature

--- python-scripts-completed-in-class/parseGFF.py
+++ python-scripts-completed-in-class/parseGFF.py
@@ -15,13 +15,37 @@ parser.add_argument("fasta", help="Name of the FASTA file to parse", type=str)
 args = parser.parse_args()
 
 # open the GFF file
-with open(args.gff) as x:
+with open(args.gff) as beyonce:
 
 	# loop over all the lines in the file
-	for line in x:
-		print(line)
+	for line in beyonce:
+		
+		# skip blank lines
+		if not line.strip():
+			continue
 
+		# else it's not a blank line
+		else:
+			line = line.strip()
 
+			# split line on tab character
+			columns      = line.split('\t')
+
+			# give variable names to the columns
+			organsim     = columns[0]
+			source       = columns[1]
+			feature_type = columns[2]
+			start        = int(columns[3])
+			end          = int(columns[4])
+			length       = columns[5]
+			strand       = columns[6]
+			attributes   = columns[8]
+
+			# add the length to column 5
+			columns[5] = str(end - start + 1)
+
+			new_line = "\t".join(columns)
+			print(new_line)
 
 
 
